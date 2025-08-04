9d8151129f524f4ba2edc553a6e5b5e283bedff7
TheSamVil <svillav4@eafit.edu.co>
2025-08-04 18:54:06 -0500

    Actividad 2 completada

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# FCFS
+# Simulacion de planificacion de procesos en un Mainframe
+Actividad 2 S.O.
+
+Samuel Villa
+
+## Instrucciones:
+![instrucciones](instrucciones.png)
+
+![requisitos](requisitos.png)
--- main.cpp
+++ main.cpp
@@ -2,78 +2,194 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
-struct Process {
+struct Proceso {
     int id;
-    int arrival_time;
-    int execution_time;
-    int priority;
-    int completion_time;
-    int turnaround_time;
-    int waiting_time;
+    int tiempo_llegada;
+    int tiempo_ejecucion;
+    int posicion;
+    int tiempo_finalizacion;
+    int tiempo_retorno;
+    int tiempo_espera;
+    bool atendido = false;
 };
 
-// Sort by arrival time for FCFS
-bool compareArrival(Process a, Process b) {
-    return a.arrival_time < b.arrival_time;
+// Ordenar por tiempo de llegada para FCFS
+bool ordenarPorLlegada(Proceso a, Proceso b) {
+    return a.tiempo_llegada < b.tiempo_llegada;
+}
+
+// FCFS (First-Come, First-Served)
+void FCFS(vector<Proceso> procesos) {
+    sort(procesos.begin(), procesos.end(), ordenarPorLlegada);
+
+    int tiempo_actual = 0;
+    double total_retorno = 0, total_espera = 0;
+    vector<int> secuencia;
+
+    for (auto &p : procesos) {
+        if (tiempo_actual < p.tiempo_llegada)
+            tiempo_actual = p.tiempo_llegada;
+
+        tiempo_actual += p.tiempo_ejecucion;
+        p.tiempo_finalizacion = tiempo_actual;
+        p.tiempo_retorno = p.tiempo_finalizacion - p.tiempo_llegada;
+        p.tiempo_espera = p.tiempo_retorno - p.tiempo_ejecucion;
+
+        total_retorno += p.tiempo_retorno;
+        total_espera += p.tiempo_espera;
+        secuencia.push_back(p.id);
+    }
+
+    cout << "\n--- Planificacion FCFS ---\n";
+    cout << "Secuencia de ejecucion: ";
+    for (size_t i = 0; i < secuencia.size(); i++) {
+        cout << "P" << secuencia[i];
+        if (i < secuencia.size() - 1) cout << " -> ";
+    }
+    cout << "\nTiempo promedio de espera: " << (total_espera / procesos.size()) << " unidades";
+    cout << "\nTiempo promedio de retorno: " << (total_retorno / procesos.size()) << " unidades\n";
+}
+
+// SSTF (Shortest Seek Time First)
+void SSTF(vector<Proceso> procesos, int posicion_inicial) {
+    int n = procesos.size();
+    int tiempo_actual = 0, completados = 0;
+    double total_retorno = 0, total_espera = 0;
+    vector<int> secuencia;
+    int cabezal = posicion_inicial;
+
+    while (completados < n) {
+        int indice = -1;
+        int menor_distancia = 1e9;
+
+        for (int i = 0; i < n; i++) {
+            if (!procesos[i].atendido && procesos[i].tiempo_llegada <= tiempo_actual) {
+                int distancia = abs(procesos[i].posicion - cabezal);
+                if (distancia < menor_distancia) {
+                    menor_distancia = distancia;
+                    indice = i;
+                }
+            }
+        }
+
+        if (indice == -1) {
+            tiempo_actual++;
+            continue;
+        }
+
+        procesos[indice].atendido = true;
+        cabezal = procesos[indice].posicion;
+
+        if (tiempo_actual < procesos[indice].tiempo_llegada)
+            tiempo_actual = procesos[indice].tiempo_llegada;
+
+        tiempo_actual += procesos[indice].tiempo_ejecucion;
+        procesos[indice].tiempo_finalizacion = tiempo_actual;
+        procesos[indice].tiempo_retorno = procesos[indice].tiempo_finalizacion - procesos[indice].tiempo_llegada;
+        procesos[indice].tiempo_espera = procesos[indice].tiempo_retorno - procesos[indice].tiempo_ejecucion;
+
+        total_retorno += procesos[indice].tiempo_retorno;
+        total_espera += procesos[indice].tiempo_espera;
+        secuencia.push_back(procesos[indice].id);
+        completados++;
+    }
+
+    cout << "\n--- Planificacion SSTF ---\n";
+    cout << "Secuencia de ejecucion: ";
+    for (size_t i = 0; i < secuencia.size(); i++) {
+        cout << "P" << secuencia[i];
+        if (i < secuencia.size() - 1) cout << " -> ";
+    }
+    cout << "\nTiempo promedio de espera: " << (total_espera / n) << " unidades";
+    cout << "\nTiempo promedio de retorno: " << (total_retorno / n) << " unidades \n";
+}
+
+// SCAN (Elevator Algorithm), cpacidad del disco limitada a 100, SCAN clasico hacia la derecha con extremos del disco
+void SCAN(vector<Proceso> procesos, int posicion_inicial, bool direccion_derecha, int disco_max) {
+    int n = procesos.size();
+    int tiempo_actual = 0, completados = 0;
+    double total_retorno = 0, total_espera = 0;
+    vector<int> secuencia;
+    int cabezal = posicion_inicial;
+
+    while (completados < n) {
+        vector<int> candidatos;
+        for (int i = 0; i < n; i++) {
+            if (!procesos[i].atendido && procesos[i].tiempo_llegada <= tiempo_actual) {
+                if ((direccion_derecha && procesos[i].posicion >= cabezal) ||
+                    (!direccion_derecha && procesos[i].posicion <= cabezal)) {
+                    candidatos.push_back(i);
+                }
+            }
+        }
+
+        if (candidatos.empty()) {
+            int distancia = direccion_derecha ? (disco_max - cabezal) : cabezal;
+            tiempo_actual += distancia;
+            cabezal = direccion_derecha ? disco_max : 0;
+            direccion_derecha = !direccion_derecha;
+            continue;
+        }
+
+        if (direccion_derecha) {
+            sort(candidatos.begin(), candidatos.end(), [&](int a, int b) {
+                return procesos[a].posicion < procesos[b].posicion;
+            });
+        } else {
+            sort(candidatos.begin(), candidatos.end(), [&](int a, int b) {
+                return procesos[a].posicion > procesos[b].posicion;
+            });
+        }
+
+        int indice = candidatos.front();
+        procesos[indice].atendido = true;
+        cabezal = procesos[indice].posicion;
+
+        if (tiempo_actual < procesos[indice].tiempo_llegada)
+            tiempo_actual = procesos[indice].tiempo_llegada;
+
+        tiempo_actual += procesos[indice].tiempo_ejecucion;
+        procesos[indice].tiempo_finalizacion = tiempo_actual;
+        procesos[indice].tiempo_retorno = procesos[indice].tiempo_finalizacion - procesos[indice].tiempo_llegada;
+        procesos[indice].tiempo_espera = procesos[indice].tiempo_retorno - procesos[indice].tiempo_ejecucion;
+
+        total_retorno += procesos[indice].tiempo_retorno;
+        total_espera += procesos[indice].tiempo_espera;
+        secuencia.push_back(procesos[indice].id);
+        completados++;
+    }
+
+    cout << "\n--- Planificacion SCAN ---\n";
+    cout << "Secuencia de ejecucion: ";
+    for (size_t i = 0; i < secuencia.size(); i++) {
+        cout << "P" << secuencia[i];
+        if (i < secuencia.size() - 1) cout << " -> ";
+    }
+    cout << "\nTiempo promedio de espera: " << (total_espera / n) << " unidades";
+    cout << "\nTiempo promedio de retorno: " << (total_retorno / n) << " unidades \n";
 }
 
 int main() {
     int n;
-    cout << "Enter number of processes: ";
+    cout << "Ingrese el numero de procesos: ";
     cin >> n;
 
-    vector<Process> processes(n);
-
-    cout << "Enter processes (id arrival_time execution_time priority):\n";
-    for(int i = 0; i < n; i++) {
-        cin >> processes[i].id >> processes[i].arrival_time >> processes[i].execution_time >> processes[i].priority;
+    vector<Proceso> procesos(n);
+    cout << "Ingrese los procesos (ID TiempoLlegada TiempoEjecucion Posicion):\n";
+    for (int i = 0; i < n; i++) {
+        cin >> procesos[i].id >> procesos[i].tiempo_llegada >> procesos[i].tiempo_ejecucion >> procesos[i].posicion;
     }
 
-    sort(processes.begin(), processes.end(), compareArrival);
+    int posicion_inicial;
+    cout << "Ingrese la posicion inicial del cabezal: ";
+    cin >> posicion_inicial;
 
-    queue<Process*> readyQueue;
-    int currentTime = 0, completed = 0;
-    double totalTurnaround = 0;
-
-    int i = 0; // index for arrival tracking
-
-    while(completed < n) {
-        // Add newly arrived processes to queue
-        while(i < n && processes[i].arrival_time <= currentTime) {
-            readyQueue.push(&processes[i]);
-            i++;
-        }
-
-        if(!readyQueue.empty()) {
-            Process* p = readyQueue.front();
-            readyQueue.pop();
-
-            if(currentTime < p->arrival_time)
-                currentTime = p->arrival_time;
-
-            currentTime += p->execution_time;
-            p->completion_time = currentTime;
-            p->turnaround_time = p->completion_time - p->arrival_time;
-            p->waiting_time = p->turnaround_time - p->execution_time;
-
-            totalTurnaround += p->turnaround_time;
-            completed++;
-        } else {
-            currentTime++;
-        }
-    }
-
-    cout << "\nID\tArrival\tExec\tPriority\tCompletion\tTurnaround\tWaiting\n";
-    for(auto &p : processes) {
-        cout << p.id << "\t" << p.arrival_time << "\t" 
-             << p.execution_time << "\t" << p.priority << "\t\t"
-             << p.completion_time << "\t\t" << p.turnaround_time 
-             << "\t\t" << p.waiting_time << "\n";
-    }
-
-    cout << "\nMean Turnaround Time = " << (totalTurnaround / n) << endl;
+    FCFS(procesos);
+    SSTF(procesos, posicion_inicial);
+    SCAN(procesos, posicion_inicial, true, 100);
 
     return 0;
 }
